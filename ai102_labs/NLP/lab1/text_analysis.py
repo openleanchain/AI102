2b2f26f39fe4e16b6201407d89dcdc5faf2d8d64
LeanChain <104700805+openleanchain@users.noreply.github.com>
2025-08-08 23:47:08 -0400

    Add files via upload

---
+++ ai102_labs/NLP/lab1/text_analysis.py
@@ -0,0 +1,71 @@
+import os
+from common.auth.credential_loader import AzureEnvLoader
+from pathlib import Path
+
+# Get credentials from environment variables
+loader = AzureEnvLoader()
+
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+
+
+def main():
+    try:
+        # Get Configuration Settings
+        ai_endpoint = loader.get_variable('SINGLE_AZURE_AI_SERVICE_ENDPOINT')
+        ai_key = loader.get_variable('SINGLE_AZURE_AI_SERVICE_KEY')
+
+        # Create client using endpoint and key
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
+
+        # Analyze each text file in the reviews folder
+        lab_dir = Path(__file__).parent  # Get the directory of the lab
+        reviews_folder = lab_dir / 'reviews'
+        print("reviews folder:", reviews_folder)
+        print("Folder exists:", reviews_folder.exists())
+
+        for file_name in os.listdir(reviews_folder):
+            # Read the file contents
+            print('\n-------------\n' + file_name)
+            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            print('\n' + text)
+
+            # Get language
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
+
+            # Get sentiment
+            sentimentAnalysis = ai_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
+
+            # Get key phrases
+            phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\nKey Phrases:")
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
+
+            # Get entities
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
+
+            # Get linked entities
+            entities = ai_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nLinks")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
+
+
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
+    
